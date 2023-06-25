720710a932188d6e9c3ec79526c21f3a99c1eea3
Moises <mcasaverde@unsa.edu.pe>
2023-06-25 17:45:22 -0500

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,319 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <algorithm>
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 800;
+
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec2 aTexCoord;\n"
+    "out vec2 TexCoord;\n"
+    "uniform mat4 model;\n"
+    "uniform mat4 view;\n"
+    "uniform mat4 projection;\n"
+    "void main()\n"
+    "{\n"
+    "gl_Position = projection * view * model * vec4(aPos, 1.0);\n"
+    "TexCoord = vec2(aTexCoord.x, aTexCoord.y);\n"
+    "}\0";
+
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "uniform vec4 ourColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = ourColor;\n"
+    "}\n\0";
+
+
+int main()
+{
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+
+    // build and compile our shader program
+    // ------------------------------------
+    // vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // link shaders
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float vertices[] = {
+        0.3f, 0.3f, 0.3f,
+        0.3f, 0.3f, -0.3f,
+        0.3f, -0.3f, 0.3f,
+        0.3f, -0.3f, -0.3f,
+        -0.3f, 0.3f, 0.3f,
+        -0.3f, 0.3f, -0.3f,
+        -0.3f, -0.3f, 0.3f,
+        -0.3f, -0.3f, -0.3f,
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 2,  // first Triangle
+        1, 2, 3,
+        0, 1, 4,
+        1, 4, 5,
+        4, 5, 6,
+        5, 6, 7,
+        2, 6, 7,
+        2, 3, 7,
+        0, 2, 6,
+        0, 4, 6,
+        1, 3, 7,
+        1, 5, 7
+    };
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0); 
+
+	int side1 [] = {0, 1, 2, 3};
+	int side2 [] = {0, 1, 4, 5};
+	int side3 [] = {4, 5, 6, 7};
+	int side4 [] = {2, 3, 6, 7};
+	int side5 [] = {0, 2, 4, 6};
+	int side6 [] = {1, 3, 5, 7};
+    glm::mat4 prev_transform = glm::mat4(1.0f);
+
+    // uncomment this call to draw in wireframe polygons.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // render loop
+    // -----------
+    glEnable(GL_DEPTH_TEST); 
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        double  timeValue = glfwGetTime();
+        // draw our first triangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        //glDrawArrays(GL_TRIANGLES, 0, 6);
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        glm::mat4 transform = prev_transform; // make sure to initialize matrix to identity matrix first
+
+        if (glfwGetKey(window,GLFW_KEY_UP) == GLFW_PRESS)
+        {
+            transform = glm::rotate(transform, 0.0005f, glm::vec3(1.0, 0.0, 0.0));
+        }
+        if (glfwGetKey(window,GLFW_KEY_DOWN) == GLFW_PRESS)
+        {
+            transform = glm::rotate(transform, 0.0005f, glm::vec3(-1.0, 0.0, 0.0));
+        }
+        if (glfwGetKey(window,GLFW_KEY_RIGHT) == GLFW_PRESS)
+        {
+            transform = glm::rotate(transform, 0.0005f, glm::vec3(0.0, -1.0, 0.0));
+        }
+        if (glfwGetKey(window,GLFW_KEY_LEFT) == GLFW_PRESS)
+        {
+            transform = glm::rotate(transform, 0.0005f, glm::vec3(0.0, 1.0, 0.0));
+        }
+        if (glfwGetKey(window,GLFW_KEY_F) == GLFW_PRESS)
+        {
+            transform = glm::rotate(transform, 0.0005f, glm::vec3(0.0, 0.0, 1.0));
+        }
+        if (glfwGetKey(window,GLFW_KEY_G) == GLFW_PRESS)
+        {
+            transform = glm::rotate(transform, 0.0005f, glm::vec3(0.0, 0.0, -1.0));
+        }
+
+        glm::mat4 view          = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        glm::mat4 projection    = glm::mat4(1.0f);
+        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        view       = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        unsigned int projloc = glGetUniformLocation(shaderProgram, "projection");
+        unsigned int viewloc = glGetUniformLocation(shaderProgram, "view");
+
+        glUniformMatrix4fv(projloc, 1, GL_FALSE, glm::value_ptr(projection));
+        glUniformMatrix4fv(viewloc, 1, GL_FALSE, glm::value_ptr(view));
+
+        
+        prev_transform = transform;
+		unsigned int transformLoc = glGetUniformLocation(shaderProgram, "model");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+        
+		glm::vec4 new1 = transform * glm::vec4(vertices[0], vertices[1], vertices[2], 1.0f);
+		glm::vec4 new2 = transform * glm::vec4(vertices[3], vertices[4], vertices[5], 1.0f);
+		glm::vec4 new3 = transform * glm::vec4(vertices[6], vertices[7], vertices[8], 1.0f);
+		glm::vec4 new4 = transform * glm::vec4(vertices[9], vertices[10], vertices[11], 1.0f);
+		glm::vec4 new5 = transform * glm::vec4(vertices[12], vertices[13], vertices[14], 1.0f);
+		glm::vec4 new6 = transform * glm::vec4(vertices[15], vertices[16], vertices[17], 1.0f);
+		glm::vec4 new7 = transform * glm::vec4(vertices[18], vertices[19], vertices[20], 1.0f);
+		glm::vec4 new8 = transform * glm::vec4(vertices[21], vertices[22], vertices[23], 1.0f);
+
+		glm::vec4 news [] = {new1, new2, new3, new4, new5, new6, new7, new8};
+
+		float zsum1 = news[side1[0]][2] + news[side1[1]][2] + news[side1[2]][2];
+		float zsum2 = news[side2[0]][2] + news[side2[1]][2] + news[side2[2]][2];
+		float zsum3 = news[side3[0]][2] + news[side3[1]][2] + news[side3[2]][2];
+		float zsum4 = news[side4[0]][2] + news[side4[1]][2] + news[side4[2]][2];
+		float zsum5 = news[side5[0]][2] + news[side5[1]][2] + news[side5[2]][2];
+		float zsum6 = news[side6[0]][2] + news[side6[1]][2] + news[side6[2]][2];
+
+		float zsums [] = {zsum1,zsum2,zsum3,zsum4,zsum5,zsum6};
+		std::sort(zsums, zsums + 6);
+
+        
+	
+        glUniform4f(vertexColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    
+        glUniform4f(vertexColorLocation, 0.0f, 1.0f, 0.0f, 1.0f);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)(6 * sizeof(float)));
+    
+        glUniform4f(vertexColorLocation, 1.0f, 0.7f, 0.0f, 1.0f);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)(12 * sizeof(float)));
+    
+        glUniform4f(vertexColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)(18 * sizeof(float)));
+    
+        glUniform4f(vertexColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)(24 * sizeof(float)));
+    
+        glUniform4f(vertexColorLocation, 1.0f, 1.0f, 0.0f, 1.0f);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)(30 * sizeof(float)));
+			
+	    
+
+
+        // glBindVertexArray(0); // no need to unbind it every time 
+ 
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
